9e0d696a650f4b342c38a677aea7bbf0ffeb4459
Luis_PC\luisa <Luis>
2020-12-03 21:36:47 +0100

    f

--- python/examples/randomColor.py
+++ python/examples/randomColor.py
@@ -10,7 +10,7 @@ LED_COUNT      = 144     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 1     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -91,12 +91,12 @@ if __name__ == '__main__':
         while True:
             print ('Start showing random Colors')
             #randomColor(strip, Color(random.randint(0,255),random.randint(0,255), random.randint(0,255)))
-            #randomColorMatrix(strip)
-            strobo(strip)
+            randomColorMatrix(strip)
+            #strobo(strip)
             time.sleep(2)
 
     except KeyboardInterrupt:
         if args.clear:
             #randomColor(strip, Color(0,0,0), 10)
-            #randomColorMatrix(strip)
-            strobo(strip)
+            randomColorMatrix(strip)
+            #strobo(strip)
